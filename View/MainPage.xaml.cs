2eada1555b5c01b49065d6ca20a9ec2cdeefec01
323 <323@323-6>
2022-09-02 12:18:31 +0300

    Добавьте файлы проекта.

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1.View;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    
+    
+
+       
+  
+}
+
